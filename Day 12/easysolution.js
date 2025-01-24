86fec75259a39444266bcaf28729ea9ade0ed028
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2025-01-24 22:10:30 +0530

    Day 12 easy of JS

---
+++ Day 12/easysolution.js
@@ -0,0 +1,17 @@
+var chunk = function(arr, size) {
+        const numberOfSubArray = Math.ceil(parseFloat(arr.length/size))
+        console.log(numberOfSubArray)
+        const chunkedArray = []
+                ind=0
+            for(let i =0; i<numberOfSubArray;i++){
+                let subChunk = []
+                for(let j=0; j <size; j++){
+                    if(arr[ind]!=undefined){
+                        subChunk.push(arr[ind])
+                    }
+                    ind++
+                }
+                chunkedArray.push(subChunk)
+            }
+        return chunkedArray
+};
